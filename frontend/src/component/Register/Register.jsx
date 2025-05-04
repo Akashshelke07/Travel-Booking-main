3352505a908bb0cad538ddc60976df5a19a1acb4
Akash Shelke <akashshelke594@gmail.com>
2025-05-04 19:36:35 +0530

    Update Register.jsx

--- frontend/src/component/Register/Register.jsx
+++ frontend/src/component/Register/Register.jsx
@@ -15,7 +15,7 @@ function Register({ setIsLoggedIn }) {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/register', {
+      const response = await fetch('http://holiday-backend-delta.vercel.app/api/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
