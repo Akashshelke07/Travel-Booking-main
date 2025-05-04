0cbbc53160ca9ae0218d8554dcc0a5b2a51ce0bc
Akash Shelke <akashshelke594@gmail.com>
2025-05-04 19:35:49 +0530

    Update Login.jsx

--- frontend/src/component/Login/Login.jsx
+++ frontend/src/component/Login/Login.jsx
@@ -16,7 +16,7 @@ function Login({ setIsLoggedIn }) {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
+      const response = await fetch('http://holiday-backend-delta.vercel.app/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
